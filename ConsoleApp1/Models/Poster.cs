a4f5c01f92054d896c3e08feff1ee0b84b6a9ba1
EslamElRefaya <eslamhmoudrfay14@gmail.com>
2025-12-14 20:28:54 +0200

    I Add Class 'Poster'

---
+++ ConsoleApp1/Models/Poster.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class Poster
+    {
+        public  int  PosterId { get; set; }
+        public  String  PosterName { get; set; }
+        public  byte[]  PosterURL { get; set; }
+    }
+}
