3758e22f81bc6ab21a87126a998ad83f1700584e
EslamElRefaya <eslamhmoudrfay14@gmail.com>
2025-12-14 21:34:14 +0200

    Remove All Useings from All Classes

--- ConsoleApp1/Models/Host.cs
+++ ConsoleApp1/Models/Host.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
-
 namespace ConsoleApp1.Models
 {    
     ///[Table("MyHost")]
