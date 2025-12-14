df8959166560f4505a2cd662e2f730d2054c65d8
EslamElRefaya <eslamhmoudrfay14@gmail.com>
2025-12-14 20:34:42 +0200

    Add Folder Helpers  inside class "Test"

---
+++ ConsoleApp1/Helpers/Test.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Helpers
+{
+   public class Test
+    {
+        public int t1 { get; set; }
+        public int t2 { get; set; }
+        public int t3 { get; set; }
+    }
+}
